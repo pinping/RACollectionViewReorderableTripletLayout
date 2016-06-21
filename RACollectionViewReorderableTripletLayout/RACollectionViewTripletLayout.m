9e92d7c8629f83b8c4d9d499fc812be55598b596
pinping <377795496@163.com>
2016-06-21 11:01:46 +0800

    修改符合个人资料6张图片的模式
    修改符合个人资料6张图片的模式

--- RACollectionViewReorderableTripletLayout/RACollectionViewTripletLayout.m
+++ RACollectionViewReorderableTripletLayout/RACollectionViewTripletLayout.m
@@ -199,23 +199,37 @@
     CGFloat rightSideLargeCellOriginX = _collectionViewSize.width - _largeCellSize.width - _insets.right;
     CGFloat rightSideSmallCellOriginX = _collectionViewSize.width - _smallCellSize.width - _insets.right;
     
-    if (indexPath.item % 6 == 0) {
+    if (indexPath.item == 0) {
         attribute.frame = CGRectMake(_insets.left, lineOriginY, _largeCellSize.width, _largeCellSize.height);
-    }else if ((indexPath.item + 1) % 6 == 0) {
-        attribute.frame = CGRectMake(rightSideLargeCellOriginX, lineOriginY, _largeCellSize.width, _largeCellSize.height);
-    }else if (line % 2 == 0) {
-        if (indexPath.item % 2 != 0) {
-            attribute.frame = CGRectMake(rightSideSmallCellOriginX, lineOriginY, _smallCellSize.width, _smallCellSize.height);
-        }else {
-            attribute.frame =CGRectMake(rightSideSmallCellOriginX, lineOriginY + _smallCellSize.height + _itemSpacing, _smallCellSize.width, _smallCellSize.height);
-        }
-    }else {
-        if (indexPath.item % 2 != 0) {
-            attribute.frame = CGRectMake(_insets.left, lineOriginY, _smallCellSize.width, _smallCellSize.height);
-        }else {
-            attribute.frame =CGRectMake(_insets.left, lineOriginY + _smallCellSize.height + _itemSpacing, _smallCellSize.width, _smallCellSize.height);
-        }
+    }else if (indexPath.item == 1) {
+        attribute.frame = CGRectMake(rightSideSmallCellOriginX, lineOriginY, _smallCellSize.width, _smallCellSize.height);
+    }else if (indexPath.item == 2) {
+        attribute.frame =CGRectMake(rightSideSmallCellOriginX, lineOriginY + _smallCellSize.height + _itemSpacing, _smallCellSize.width, _smallCellSize.height);
+    }else if (indexPath.item == 3) {
+        attribute.frame =CGRectMake(_insets.left, lineOriginY , _smallCellSize.width, _smallCellSize.height);
+    }else if (indexPath.item == 4) {
+        attribute.frame =CGRectMake(_insets.left + _smallCellSize.width + _lineSpacing, lineOriginY , _smallCellSize.width, _smallCellSize.height);
+    }else if (indexPath.item == 5) {
+        attribute.frame =CGRectMake(rightSideSmallCellOriginX, lineOriginY, _smallCellSize.width, _smallCellSize.height);
     }
+    
+    // if (indexPath.item % 6 == 0) {
+    //     attribute.frame = CGRectMake(_insets.left, lineOriginY, _largeCellSize.width, _largeCellSize.height);
+    // }else if ((indexPath.item + 1) % 6 == 0) {
+    //     attribute.frame = CGRectMake(rightSideLargeCellOriginX, lineOriginY, _largeCellSize.width, _largeCellSize.height);
+    // }else if (line % 2 == 0) {
+    //     if (indexPath.item % 2 != 0) {
+    //         attribute.frame = CGRectMake(rightSideSmallCellOriginX, lineOriginY, _smallCellSize.width, _smallCellSize.height);
+    //     }else {
+    //         attribute.frame =CGRectMake(rightSideSmallCellOriginX, lineOriginY + _smallCellSize.height + _itemSpacing, _smallCellSize.width, _smallCellSize.height);
+    //     }
+    // }else {
+    //     if (indexPath.item % 2 != 0) {
+    //         attribute.frame = CGRectMake(_insets.left, lineOriginY, _smallCellSize.width, _smallCellSize.height);
+    //     }else {
+    //         attribute.frame =CGRectMake(_insets.left, lineOriginY + _smallCellSize.height + _itemSpacing, _smallCellSize.width, _smallCellSize.height);
+    //     }
+    // }
     return attribute;
 }
 
